76c8cfa1060a81d6639668cf40956b7d8563c33d
SolNegro <bruno.santos16264@a.fecaf.com.br>
2021-04-10 20:40:09 -0300

    Atualização 1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const config = require('config')
+const pg = require('pg')
+const { response } = require('express')
 const port = process.env.PORT || config.get("server.port")
 
 const app = express()
@@ -7,18 +9,53 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 const listaPedidos = []
+const pool = new pg.Pool ({
+    connectionString: "postgres://zwgrdipbywmvkk:3f1c63b292a130d8047468bf20e54cd285dc4bc7b47530f2317e9b05b3d353f8@ec2-54-164-241-193.compute-1.amazonaws.com:5432/da6ufsvtl3eev7",
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+app.route('/reset').get((req, res) => { 
+    let qry = "DROP TABLE IF EXISTS pedidos;"
+    qry+= "CREATE TABLE pedidos ("
+    qry+= "cliente char(100),"
+    qry+= "sabor char(50),"
+    qry+= "quantidade int,"
+    qry+= "tamanho char(25)"
+    qry+= ");"
+    pool.query(qry, (err, dbres) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send("Banco de dados resetado")
+        }
+    })
+})
 
 app.route('/pedidos').get((req, res) => {
     console.log("/pedidos acionado")
-    res.status(200).json(listaPedidos)
+    let qry = "SELECT * FROM pedidos;"
+    pool.query(qry, (err, dbres) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).json(dbres.rows)
+        }
+    });
 })
 
-app.route('/pedidos/adicionar').post((req, res) => {
-    console.log("Headers =>", req.headers)
-    console.log("Query =>", req.query)
-    console.log("Body =>", req.body)
-    listaPedidos.push(req.body)
-    res.status(200).send("Pedido adicionado com sucesso")
+app.route('/pedido/adicionar').post((req, res) => {
+    console.log("/pedido/adicionar acionado")
+    let qry = "INSERT INTO pedidos (cliente, sabor, quantidade, tamanho)"
+    qry += ` VALUES ('${req.body.cliente}', '${req.body.sabor}', ${req.body.quantidade}, '${res.body.tamanho}'); `
+    pool.query(qry, (err, dbres) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send("Pedido adicionado com sucesso")
+        }
+    });
 })
 
 app.listen(port, () => {
